012570c277e4e8a86a4e57c41ed4e90fa3e89936
ohuss1 <ocompu@outlook.com>
2020-03-08 12:38:42 -0400

    imports

---
+++ TweetPrinter.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import sys
+import bs4
+import string
+driver=webdriver.Firefox(executable_path=r'C:\Users\OComp\Desktop\geckodriver-v0.23.0-win64\geckodriver.exe'
+                         )#replace the path with your gecko exe path
