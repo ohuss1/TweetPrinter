88ca614952753488153064013ee551658f65259f
ohuss1 <ocompu@outlook.com>
2020-03-08 12:54:57 -0400

    userinput

--- TweetPrinter.py
+++ TweetPrinter.py
@@ -6,3 +6,13 @@ import bs4
 import string
 driver=webdriver.Firefox(executable_path=r'C:\Users\OComp\Desktop\geckodriver-v0.23.0-win64\geckodriver.exe'
                          )#replace the path with your gecko exe path
+print('Please enter the twitter url')
+UrlString=raw_input()
+#print(UrlString)
+driver.get(UrlString)
+time.sleep(1)
+numpages=3;
+print('please enter number of pages you would like to print')
+numpages=(int(raw_input()))
+#print(numpages)
+#print(type(numpages))
